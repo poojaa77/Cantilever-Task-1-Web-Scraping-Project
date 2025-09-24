f2c15be7a32181ade86c7a61fddc60795173143f
Pooja Gajjar <145318255+poojaa77@users.noreply.github.com>
2025-09-24 19:07:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -324,4 +324,5 @@ if __name__ == '__main__':
     print("Directories created/verified")
     print("Starting server on http://localhost:5000")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+    app.run(debug=True, port=5000)
